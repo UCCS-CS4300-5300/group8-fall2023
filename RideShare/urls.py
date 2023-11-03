b7820ae0ca9835b041becb60547b04db13967ca3
Replit user <>
2023-11-03 02:28:49 +0000

    Make a Vehicle Map View

--- RideShare/urls.py
+++ RideShare/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  
-
+  path('vehicleMap/', views.VehicleMapView.as_view(), name='vehicleMap'),
 ]
--- RideShare/views.py
+++ RideShare/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 
 
 def home(request):
   context = {}
-  return render(request, 'home.html')
+  return render(request, 'home.html', context)
+
+class VehicleMapView(View):
+  template_name = 'vehicleMap.html'
+
+  def get(self, request):
+    context = {}
+    context['key'] = 'AIzaSyD-oTBt9sdMhCXyQqrtuok0CYvP7ev58hg'
+      
+    return render(request, self.template_name, context)  
