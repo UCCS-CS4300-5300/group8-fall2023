7e86c695d68bd479d951ed0e29fa22896338812c
BenHNet <ben@htechinc.com>
2023-11-04 04:03:32 +0000

    Getting everything working together.

--- SignUpSignIn/urls.py
+++ SignUpSignIn/urls.py
@@ -4,5 +4,6 @@ from . import views as my_views
 
 urlpatterns = [
     path('signup/', my_views.signuppage, name='signuppage'),
-    path('signin/', my_views.signinpage, name='signinpage')
+    path('signin/', my_views.signinpage, name='signinpage'),
+    path('logout/', my_views.logoutpage, name='logoutpage')
 ]
