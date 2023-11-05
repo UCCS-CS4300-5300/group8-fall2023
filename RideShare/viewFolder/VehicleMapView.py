1b5391335b92fba32b4c8d10649c917bb6c43c01
jwilli-uccs <143824801+jwilli-uccs@users.noreply.github.com>
2023-11-05 00:19:10 +0000

    added vehicle map

---
+++ RideShare/viewFolder/VehicleMapView.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.views import View
+from RideShare.modelsFolder.VehicleModel import Vehicle
+
+class VehicleMapView(View):
+  template_name = 'vehicleMap.html'
+
+  def get(self, request):
+    context = {}
+
+    vehicles = Vehicle.objects.all()
+
+    """"
+    serializers is not working because djangorestframework package will not install
+    serialized_data = [
+      VehicleSerializer(vehicle).data for vehicle in vehicles
+    ]
+    """
+
+    serialized_data = [
+      {"id": vehicle.id, 
+       "type": vehicle.type, 
+       "latitude": vehicle.latitude,
+       "longitude": vehicle.longitude,
+       # note that booleans have to be converted to strings for JSON.parse
+       "isAvailable": str("{}").format(vehicle.isAvailable)
+      } 
+      for vehicle in vehicles
+    ]
+
+    response_data = {
+      'count': len(serialized_data),
+      'results': serialized_data
+    }
+
+    context['key'] = 'AIzaSyD-oTBt9sdMhCXyQqrtuok0CYvP7ev58hg'
+    context['vehicles'] = vehicles
+    context['response_data'] = response_data
+
+    return render(request, self.template_name, context)  
