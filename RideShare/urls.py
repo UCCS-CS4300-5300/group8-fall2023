944e297ac401f65cf7726d472f102a607831ce71
dajameson <90569513+dajameson@users.noreply.github.com>
2023-11-14 01:59:41 +0000

    Fixed some stuff, instead of a post method in the vehicle map view, I just created a new view

--- RideShare/urls.py
+++ RideShare/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from RideShare.viewFolder.VehicleMapView import VehicleMapView
+from RideShare.viewFolder.VehicleMapView import VehicleMapView, CheckInView
 
 urlpatterns = [
   path('', views.home, name='home'),
   path('vehicleMap/', VehicleMapView.as_view(), name='vehicleMap'),
-  path('check-in/', VehicleMapView.as_view(), name='check_in'),
+  path('check-in/', CheckInView.as_view(), name='check_in'),
 ]
--- RideShare/viewFolder/VehicleMapView.py
+++ RideShare/viewFolder/VehicleMapView.py
@@ -4,6 +4,7 @@ from RideShare.modelsFolder.VehicleModel import Vehicle
 from RideShare.modelsFolder.VehicleModel import VehicleRental
 from django.urls import reverse
 from RideShare.forms import CheckInForm
+from django.utils import timezone
 
 
 class VehicleMapView(View):
@@ -41,11 +42,56 @@ class VehicleMapView(View):
     context['vehicles'] = vehicles
     context['response_data'] = response_data
     context['rentals'] = VehicleRental.objects.filter(user=request.user, checkinTime__isnull=True)
+    context['vehicle'] = Vehicle.objects.first()
 
     return render(request, self.template_name, context)  
 
+# David - gave up on check in with post function in MapView
+class CheckInView(View):
+  template_name = "checkin.html"
+
+  def get(self, request):
+    form = CheckInForm()
+    return render(request, self.template_name, {'form': form})
+
+  def post(self, request):
+    form = CheckInForm(request.POST)
+    if form.is_valid():
+      vehicle_id = form.cleaned_data['vehicle_id']
+      checkin_location = form.cleaned_data['checkin_location']
+
+      vehicle = Vehicle.objects.get(pk=vehicle_id)
+      rental = VehicleRental.objects.filter(user=request.user, vehicle=vehicle, checkinTime__isnull=True).first()
+
+      if rental: 
+        rental.checkin_location = checkin_location
+        rental.checkin_time = timezone.now()
+        rental.save()
+
+        # Update the availability of the vehicle
+        vehicle.is_available = True
+        vehicle.save()
+
+        return redirect('vehicleMap')
+      print("Rental not found or form data invalid")
+      print("Form errors: ", form.errors)
+
+      # handle invalid form data
+    context = {
+      'form': form,
+      'key': 'AIzaSyD-oTBt9sdMhCXyQqrtuok0CYvP7ev58hg',
+      'vehicles': Vehicle.objects.all(),
+      'rentals': VehicleRental.objects.filter(user=request.user, checkinTime__isnull=True),
+      }
+    
+    print("form errors: ", form.errors)
+    print("Redirecting to 'vehicle_map' failed. Form is not valid or rental is not found.")
+    return render(request, self.template_name, context)
+
+
 
   
+  '''
   def post(self, request):
     form = CheckInForm(request.POST)
     if form.is_valid():
@@ -57,7 +103,6 @@ class VehicleMapView(View):
 
       if rental: 
         rental.checkin_location = checkin_location
-        rental.payment_method = payment_method
         rental.checkin_time = timezone.now()
         rental.save()
 
@@ -65,7 +110,9 @@ class VehicleMapView(View):
         vehicle.is_available = True
         vehicle.save()
 
-        return redirect('vehicle_map')
+        return redirect('vehicleMap')
+      print("Rental not found or form data invalid")
+      print("Form errors: ", form.errors)
 
       # handle invalid form data
     context = {
@@ -74,4 +121,6 @@ class VehicleMapView(View):
       'vehicles': Vehicle.objects.all(),
       'rentals': VehicleRental.objects.filter(user=request.user, checkinTime__isnull=True),
       }
-    return render(request, self.template_name, context)
+    print("form errors: ", form.errors)
+    print("Redirecting to 'vehicle_map' failed. Form is not valid or rental is not found.")
+    return render(request, self.template_name, context)'''
